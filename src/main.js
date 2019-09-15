64b8f24f263b2b8bb7f8683dc4f7b92a2d2d97fa
Ebrahim Izadi <izadi.abraham@gmail.com>
2019-09-15 19:05:10 +0430

    5th segment completed

--- src/main.js
+++ src/main.js
@@ -1,4 +1,6 @@
-
-$(window).scroll(function(){
-  $('.navbar').toggleClass('scrolled', $(this).scrollTop() > 50);
-});
+$(window).scroll(
+  function () {
+    const $navbar = $('.main-nav');
+    $navbar.toggleClass('first-segment', $(this).scrollTop() > 50);
+    $navbar.toggleClass('second-segment', $(this).scrollTop() > 890); //todo  convert px to rem
+  });
